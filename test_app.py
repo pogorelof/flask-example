a2c95df86453074d6312fcd1ebc9678a7581d0f3
Vladimir <v.pogorelof@gmail.com>
2025-03-19 15:20:53 +0500

    update

--- test_app.py
+++ test_app.py
@@ -6,11 +6,6 @@ def client():
     app.testing = True
     return app.test_client()
 
-def test_home(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert "Привет! Это простое Flask-приложение." in response.data.decode("utf-8")
-
 def test_hello(client):
     response = client.get('/hello/ChatGPT')
     assert response.status_code == 200
